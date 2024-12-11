c9ff8a1fad7bdb86ce131501bafaca78debc63e6
liuyujielol <2073201758GD@gmail.com>
2024-12-11 22:07:18 +0800

    i10n: add zh-CN to i18n assets

--- src/renderer/src/i18n.ts
+++ src/renderer/src/i18n.ts
@@ -4,12 +4,14 @@ import { initReactI18next } from "react-i18next"
 import enUS from "@assets/lang/en-US.json"
 import esES from "@assets/lang/es-ES.json"
 import ruRU from "@assets/lang/ru-RU.json"
+import zhCN from "@assets/lang/zh-CN.json"
 
 i18n.use(initReactI18next).init({
   resources: {
     "en-US": { translation: enUS, name: "English" },
     "es-ES": { translation: esES, name: "Español" },
-    "ru-RU": { translation: ruRU, name: "Русский" }
+    "ru-RU": { translation: ruRU, name: "Русский" },
+    "zh-CN": { translation: zhCN, name: "简体中文" }
   },
   lng: "en-US",
   fallbackLng: "es-ES",
