3dc3edee311cce3a9d5c27023846ab50a8f240d1
XurxoMF <xurxolaxe@gmail.com>
2024-12-12 17:18:55 +0100

    Added cs-CS localization

--- src/renderer/src/i18n.ts
+++ src/renderer/src/i18n.ts
@@ -6,6 +6,7 @@ import esES from "@assets/lang/es-ES.json"
 import ruRU from "@assets/lang/ru-RU.json"
 import zhCN from "@assets/lang/zh-CN.json"
 import frFR from "@assets/lang/fr-FR.json"
+import csCS from "@assets/lang/cs-CS.json"
 
 i18n.use(initReactI18next).init({
   resources: {
@@ -13,7 +14,8 @@ i18n.use(initReactI18next).init({
     "es-ES": { translation: esES, name: "Español", credits: "by XuroMF" },
     "ru-RU": { translation: ruRU, name: "Русский", credits: "by megabezdelnik" },
     "zh-CN": { translation: zhCN, name: "简体中文", credits: "by liuyujielol" },
-    "fr-FR": { translation: frFR, name: "Français", credits: "by LorIlcs" }
+    "fr-FR": { translation: frFR, name: "Français", credits: "by LorIlcs" },
+    "cs-CS": { translation: csCS, name: "Čeština", credits: "by DejFidOFF" }
   },
   lng: "en-US",
   fallbackLng: "es-ES",
