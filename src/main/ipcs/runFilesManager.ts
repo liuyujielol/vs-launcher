68e07a3faa5d4475a32fdbb1c2894ff091871f55
XurxoMF <xurxolaxe@gmail.com>
2024-12-17 19:52:46 +0100

    Removed the ` preventing the game from running.

--- src/main/ipcs/runFilesManager.ts
+++ src/main/ipcs/runFilesManager.ts
@@ -76,7 +76,7 @@ ipcMain.handle("execute-game", async (_event, version: InstalledGameVersionType,
       try {
         logMessage("info", `[execute-game] Executing game version ${version.version} from ${version.path}\\Vintagestory.exe with data path ${installation.path}`)
 
-        const externalApp = spawn(join(version.path, "Vintagestory.exe`"), [`--dataPath=${installation.path}`])
+        const externalApp = spawn(join(version.path, "Vintagestory.exe"), [`--dataPath=${installation.path}`])
 
         externalApp.stdout.on("data", (data) => {
           logMessage("info", `[execute-game] Game sent data: ${data}`)
